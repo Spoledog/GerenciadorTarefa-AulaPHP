3f8fad88137a5e3805821c75a4c603244802c65f
ANA PAULA DE OLIVEIRA CEDRO <cedro.0990@aluno.pr.senac.br>
2025-08-20 12:26:42 -0300

    Criando conexao/salvar vinculando o projeto com banco de dados MySQL

---
+++ save.php
@@ -0,0 +1,39 @@
+<?php
+
+require_once 'conn.php';
+
+try {
+    if($_SERVER["REQUST_METHOD"]== "POST"){
+        $title = isset($_POST['title']) ? $_POST['title'] : null;
+        $descriptionm= isset($_POST['description'])  ? $_POST['description']: null;
+
+    if ($title) {
+        $sql = "INSERT INTO crud_php (title, description) VALUES (?,?)";
+        $stmt = $conn->prepare($sql);
+
+        if($stmt) {
+            $stmt->bind_param("ss", $title, $description);
+        
+        if($stmt->execute()){
+            header("Location: index.php");
+            exit();
+        } else {
+            throw new Exception("Erro ao executar a consulta: " .$stmt->error);
+        }
+
+        $stmt->close();
+        }else{
+            throw new Exception("Erro ao preparar consulta: " .$conn->error);
+        }
+    }else {
+        throw new Exception("O campo title é obrigatório");
+    }
+    } else {
+        throw new exception("Metódo de requisição invalido");
+    }
+    } catch (Exception $e){
+        echo "Erro: " .$e->getMessage();
+    } finally {
+        $conn->close();
+    }
+
